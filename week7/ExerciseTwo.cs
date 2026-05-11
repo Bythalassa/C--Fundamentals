49dd3f6863d7498e407031cd6f49f9103d60a7e9
Daniel Dentone <143889531+Bythalassa@users.noreply.github.com>
2026-05-11 12:01:34 -0500

    Update ExerciseTwo.cs

---
+++ week7/ExerciseTwo.cs
@@ -0,0 +1,69 @@
+using JetBrains.Annotations;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+public class week7Bloquedos : MonoBehaviour
+{
+
+    public List <string> inventary = new List<string> ();
+
+    string[] enemigo = new string[4];
+    void Start()
+    {
+        enemigo[0] = "Serpiente";
+        enemigo[1] = "Oruga";
+        enemigo[2] = "Dragon";
+        enemigo[3] = "UltiPersona";
+
+        inventario();
+
+        inventary.Add("Keys");
+        inventary.Add("Green Dimonds");
+        inventary.Add("Red Dimonds");
+        inventary.Add("Sword");
+    }
+
+
+    public void enemyFind()
+    {
+        int i = 0;
+        while (i < enemigo.Length)
+        {
+            Debug.Log(enemigo[i]);
+            if (enemigo[i] == "Dragon")
+            {
+                Debug.Log("el enemigo Dragone esta en el indice" + i);
+            }
+            i++;
+        }
+    }
+
+    public void inventario()
+    {
+        int i = 0;
+        while (i < inventary.Count)
+        {
+            //Debug.Log( inventary[0], inventary[1], inventary[2]);
+            Debug.Log(item);
+            i++;
+
+        item = ?
+                : enemigo[0]
+                : enemigo[i]
+                : enemigo[i]
+
+
+            //item si el indice es 0 entonces debugeo el inventory[0], si 
+
+
+        }
+    }
+
+
+    /*void Update()
+    {
+
+    }*/
+}
+
